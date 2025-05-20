b7339be8d044c9a9f88335954095051c002f16e7
Rodrigo Acosta <acosta_rodrigo@hotmail.com>
2025-05-20 11:41:32 -0400

    config changes to build in erc2

--- config-overrides.js
+++ config-overrides.js
@@ -2,13 +2,18 @@ require("dotenv").config();
 const webpack = require("webpack");
 
 module.exports = function override(config, env) {
+  // 1) Provide global shims for process and Buffer
   config.plugins.push(
     new webpack.ProvidePlugin({
+      process: "process/browser",
       Buffer: ["buffer", "Buffer"],
     }),
     new webpack.EnvironmentPlugin(["RPC_URL", "REST_URL"])
   );
+
+  // 2) Resolve fallbacks (turn off Node polyfills you don't need)
   config.resolve.fallback = {
+    ...config.resolve.fallback,
     buffer: false,
     crypto: false,
     events: false,
@@ -16,5 +21,18 @@ module.exports = function override(config, env) {
     stream: false,
     string_decoder: false,
   };
+
+  // 3) Transpile any CommonJS modules (e.g. decode.ts) into browser-safe ESM
+  config.module.rules.push({
+    test: /decode\.ts$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [["@babel/preset-env", { targets: "defaults" }]],
+      },
+    },
+  });
+
   return config;
 };
